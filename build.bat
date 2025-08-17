d3cf15c778f7c2f895f22d6bd002f4b5158b89bc
AEMacro <michael.mitchell0828@gmail.com>
2025-08-17 13:10:53 -0400

    Updated for 217

--- build.bat
+++ build.bat
@@ -1,4 +1,3 @@
 cmake -P cmkr.cmake
-cmake -A Win32 -B build -DCMAKE_POLICY_DEFAULT_CMP0000=NEW -DCMAKE_POLICY_WARNING_CMP0000=OFF -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+cmake -A Win32 -B build -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 cmake --build build --config Release
-pause
